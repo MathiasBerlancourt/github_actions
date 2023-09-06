1971e5347ccd54b70861425782eb6a37c5d6547f
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-09-06 22:52:55 +0200

    remove import

--- file_test.py
+++ file_test.py
@@ -1,6 +1,3 @@
-import pytest
-
-# Fonction test du résultat de 2+4
 
 
 def test_calc_addition():
@@ -9,20 +6,14 @@ def test_calc_addition():
     assert output == 6
 
 
-# Fonction test du résultat de 2-4
-
 def test_calc_substraction():
     output = 2 - 4
     assert output == -2
-
-# Fonction test du résultat de 2*4
 
 
 def test_calc_multiply():
     output = 2 * 4
     assert output == 8
-
- # Fonction test si le résultat renvoie 'hello'
 
 
 def test_coucou():
